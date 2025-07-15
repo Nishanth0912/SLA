07bdcab069d9d2e04ba2cbccd30965f852be261d
Nishanth0912 <nilanishanth2000@gmail.com>
2025-07-15 22:31:16 +0530

    Delete JS directory

--- JS/BASICDOM/basicdom.js
+++
@@ -1,9 +0,0 @@
-function clickme(){
-    document.getElementById("title").textContent="Title Changed!!"
-    document.body.style.backgroundColor="lightblue";
-   
-}
-
-function changefirstpara(){
-    document.querySelector("p").textContent="this was changed"
-}
--- JS/STATEMENTS/if.js
+++
@@ -1,8 +0,0 @@
-let age=5;
-
-if (age>=18) {
-    console.log("you are eligible to vote");
-}
-else{
-    console.log("Meet the eligible age and come to vote ....");
-}
